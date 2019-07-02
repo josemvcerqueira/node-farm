22fbb61e99a5c7f2c9394407cf0364e3ba87f0e8
Jose Cerqueira <jose@origensmedia.com>
2019-07-02 01:59:14 +0100

    read, update and send the templates

--- index.js
+++ index.js
@@ -30,13 +30,57 @@ const url = require("url");
 ///////////////////////////////////////////////////////////
 // SERVER
 
+const replaceTemplate = (temp, product) => {
+	let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+	output = output.replace(/{%PRODUCT_IMAGE%}/g, product.image);
+	output = output.replace(/{%PRODUCT_QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%PRODUCT_ORIGIN%}/g, product.from);
+	output = output.replace(/{%PRODUCT_NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%PRODUCT_PRICE%}/g, product.price);
+	output = output.replace(/{%PRODUCT_DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%PRODUCT_ID%}/g, product.id);
+
+	if (!product.organic) {
+		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+	}
+
+	return output;
+};
+
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	"utf-8"
+);
+const tempCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	"utf-8"
+);
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	"utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
+
+	// Overview page
 	if (pathName === "/" || pathName === "/overview") {
-		res.end("This is the OVERVIEW");
+		res.writeHead(200, {
+			"Content-type": "text/html"
+		});
+
+		const cardsHtml = dataObj
+			.map(el => replaceTemplate(tempCard, el))
+			.join("");
+
+		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+		res.end(output);
+
+		// Product page
 	} else if (pathName === "/product") {
 		res.end("This is the PRODUCT");
 	} else if (pathName === "/api") {
@@ -44,6 +88,8 @@ const server = http.createServer((req, res) => {
 			"Content-type": "application/json"
 		});
 		res.end(data);
+
+		// API
 	} else {
 		res.writeHead(404, {
 			"Content-type": "text/html",
